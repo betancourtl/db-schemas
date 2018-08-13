edb93b73df2beff2b93b0588df25dca7a4966438
Luis Betancourt <webdeveloperpr@gmail.com>
2018-08-12 21:24:57 -0400

    Fix formatting

--- real-estate/db.sql
+++ real-estate/db.sql
@@ -42,30 +42,30 @@ CREATE TABLE property_status (
 );
 
 CREATE TABLE properties (
-  id                 INT PRIMARY KEY                                                                                                                                                                                                                                                                                                                                                                                  AUTO_INCREMENT,
-  price   DECIMAL(7, 2)  NOT NULL                                                                                                                                                                                                                                                                                                                                                                           DEFAULT 0,
-  address VARCHAR(255)  NOT NULL,
-  state   VARCHAR(2)    NOT NULL,
-  zip     VARCHAR(10), -- 00921-234
-  lat     DECIMAL(10, 8) NOT NULL,
-  lon     DECIMAL(11, 8) NOT NULL,
-  beds    INT            NOT NULL                                                                                                                                                                                                                                                                                                                                                                          DEFAULT 0,
-  baths   INT            NOT NULL                                                                                                                                                                                                                                                                                                                                                                          DEFAULT 0,
-  year_built YEAR(4)     NOT NULL,
-  heating    VARCHAR(255),
-  attic      BOOL,
-  appliances VARCHAR(255),
-  flooring   VARCHAR(255),
-  heating_type VARCHAR(255),
-  cooling      VARCHAR(255),
-  parking      VARCHAR(255),
-  parking_type VARCHAR(255),
-  mls          VARCHAR(255),
-  sqft         DECIMAL(6, 2)                                                                                                                                                                                                                                                                                                                                                                                    DEFAULT 0,
-  description  TEXT,
-  create_at    DATETIME  NOT NULL                                                                                                                                                                                                                                                                                                                                                                               DEFAULT NOW(),
-  update_at    DATETIME  NOT NULL                                                                                                                                                                                                                                                                                                                                                                               DEFAULT NOW() ON UPDATE NOW(),
-  property_type_id INT,
+  id                 INT PRIMARY KEY                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    AUTO_INCREMENT,
+  price              DECIMAL(7, 2)  NOT NULL                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            DEFAULT 0,
+  address            VARCHAR(255)   NOT NULL,
+  state              VARCHAR(2)     NOT NULL,
+  zip                VARCHAR(10), -- 00921-234
+  lat                DECIMAL(10, 8) NOT NULL,
+  lon                DECIMAL(11, 8) NOT NULL,
+  beds               INT            NOT NULL                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         DEFAULT 0,
+  baths              INT            NOT NULL                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         DEFAULT 0,
+  year_built         YEAR(4)        NOT NULL,
+  heating            VARCHAR(255),
+  attic              BOOL,
+  appliances         VARCHAR(255),
+  flooring           VARCHAR(255),
+  heating_type       VARCHAR(255),
+  cooling            VARCHAR(255),
+  parking            VARCHAR(255),
+  parking_type       VARCHAR(255),
+  mls                VARCHAR(255),
+  sqft               DECIMAL(6, 2) DEFAULT 0,
+  description        TEXT,
+  create_at          DATETIME       NOT NULL                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              DEFAULT NOW(),
+  update_at          DATETIME       NOT NULL                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              DEFAULT NOW() ON UPDATE NOW(),
+  property_type_id   INT,
   property_status_id INT,
   FOREIGN KEY (property_type_id) REFERENCES property_types (id) ON DELETE SET NULL,
   FOREIGN KEY (property_status_id) REFERENCES property_status (id) ON DELETE SET NULL
@@ -74,8 +74,8 @@ CREATE TABLE properties (
 # User can save properties that he likes
 CREATE TABLE saved_properties (
   create_at   DATETIME NOT NULL      DEFAULT NOW(),
-  update_at DATETIME NOT NULL      DEFAULT NOW() ON UPDATE NOW(),
-  user_id   INT      NOT NULL,
+  update_at   DATETIME NOT NULL      DEFAULT NOW() ON UPDATE NOW(),
+  user_id     INT      NOT NULL,
   property_id INT      NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id),
   FOREIGN KEY (property_id) REFERENCES properties (id),
@@ -84,21 +84,21 @@ CREATE TABLE saved_properties (
 
 # properties have can have multiple images
 CREATE TABLE property_images (
-  id          INT PRIMARY KEY            AUTO_INCREMENT,
-  url VARCHAR(255)   NOT NULL,
-  create_at DATETIME NOT NULL          DEFAULT NOW(),
-  update_at DATETIME NOT NULL          DEFAULT NOW() ON UPDATE NOW(),
-  property_id INT    NOT NULL,
+  id          INT PRIMARY KEY                AUTO_INCREMENT,
+  url         VARCHAR(255) NOT NULL,
+  create_at   DATETIME     NOT NULL          DEFAULT NOW(),
+  update_at   DATETIME     NOT NULL          DEFAULT NOW() ON UPDATE NOW(),
+  property_id INT          NOT NULL,
   FOREIGN KEY (property_id) REFERENCES properties (id)
 );
 
 # Save all of the property views from users, or guests?
 CREATE TABLE property_views (
   id          INT PRIMARY KEY        AUTO_INCREMENT,
-  create_at DATETIME NOT NULL      DEFAULT NOW(),
-  update_at DATETIME NOT NULL      DEFAULT NOW() ON UPDATE NOW(),
-  property_id INT    NOT NULL,
-  user_id     INT    NOT NULL,
+  create_at   DATETIME NOT NULL      DEFAULT NOW(),
+  update_at   DATETIME NOT NULL      DEFAULT NOW() ON UPDATE NOW(),
+  property_id INT      NOT NULL,
+  user_id     INT      NOT NULL,
   FOREIGN KEY (property_id) REFERENCES properties (id) ON DELETE CASCADE,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
